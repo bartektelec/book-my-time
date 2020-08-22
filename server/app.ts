ac866fcd799c068435988ad03ec123b4343476e3
bartektelec <bartektelec@gmail.com>
2020-08-23 00:00:58 +0200

    general: add dockerfile, minor fixes

--- server/app.ts
+++ server/app.ts
@@ -1,10 +1,14 @@
 import express, { Application, Request, Response } from 'express';
+import ip from 'ip';
 
 const app: Application = express();
 app.get('/', () => {
   console.log('hello there');
 });
 
-app.listen(3000, () => {
-  console.log('server listening on port 3000');
+const PORT = 3001;
+const IP = ip.address();
+
+app.listen(PORT, () => {
+  console.log(`server listening on http://${IP}:${PORT}`);
 });
