2349cdd0771b70d5974b4b82aed1335ce737fc68
szczep4niak <jjdroid6969@gmail.com>
2020-09-05 14:14:56 +0200

    sendConfEmail endpoint

--- mail/src/routes/index.ts
+++ mail/src/routes/index.ts
@@ -1,10 +1,34 @@
 import { Router, Request, Response } from 'express';
+import { env } from 'process';
 
 const router = Router();
+const transporter = require('nodemailer').createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
 
 router.get('/', (req: Request, res: Response) => {
-  // throw new Error('next error')
   return res.json({ msg: 'henlo' });
+});
+
+router.post('/sendConfirmationEmail', async (req: Request, res: Response) => {
+  try {
+    const { recipient, subject, text } = req.body;
+    const mailOptions = {
+      from: process.env.EMAIL,
+      to: recipient,
+      subject: 'Book my time event creation confirmation',
+      text: 'Oi',
+    };
+    const info = await transporter.sendMail(mailOptions);
+    res.json(info);
+  } catch (err) {
+    console.error(err);
+    res.json(err);
+  }
 });
 
 export default router;
