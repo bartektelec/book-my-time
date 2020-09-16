ddc7418e2ca4b10e7128d621be0cffe4a1d92567
szczep4niak <jjdroid6969@gmail.com>
2020-09-16 20:10:13 +0200

    Appointment column

--- ui/src/components/Calendar/HoursColumn.tsx
+++ ui/src/components/Calendar/HoursColumn.tsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import moment from 'moment';
+import styled from 'styled-components';
+import variables from '../../common/variables';
+
+const getHourArray = (hourStart: number = 8, hourEnd: number = 20): Array<string> => {
+  if (hourStart < 0 || hourStart > hourEnd) throw Error('hourStart has to be greater than 0 and lower than hourEnd');
+  if (hourEnd > 24) throw Error('hourEnd has to be lesser than 24');
+
+  const items: Array<string> = [];
+  new Array<string>(hourEnd - hourStart + 1).fill('').forEach((_, index) => {
+    items.push(moment({ hour: index + hourStart }).format('h A'));
+  });
+  return items;
+};
+
+const StickyColumn = styled.div`
+  position: sticky;
+  color: ${variables.colors.disabled};
+`;
 
 const HoursColumn: React.FC = () => {
-  return <></>;
+  return (
+    <StickyColumn>
+      {getHourArray().map((hour) => {
+        return <p key={hour}>{hour}</p>;
+      })}
+    </StickyColumn>
+  );
 };
 
 export default HoursColumn;
